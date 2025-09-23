f12c3a0109071fb9351e16a02840e427d824cee6
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-09-22 18:15:24 -0600

    Se guarda progreso de la aplicacion, porque se haran analisis de configuracion para hacer un deploy en netlify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { MainPageComponent } from './components/main-content/main-page/main-page
 import { HomePageComponent } from './components/home-page/home-page/home-page.component';
 import { FavoriteComponent } from './components/favorite/favorite.component';
 import { RandomDrinkComponent } from './components/random-drink/random-drink.component';
-import { NavPageComponent } from './components/nav-page/nav-page/nav-page.component';
 
 export const routes: Routes = [
 
-  { path: '', component: NavPageComponent},
-  { path: 'random', component: RandomDrinkComponent },
-  { path: 'favorite', component: FavoriteComponent}
+  {
+    path: '',
+    component: HomePageComponent,
+    children: [
+      { path: 'main', component: MainPageComponent},
+      { path: 'random', component: RandomDrinkComponent },
+      { path: 'favorite', component: FavoriteComponent},
+    ]
+  }
 ];
--- src/app/components/home-page/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page/home-page.component.ts
@@ -3,10 +3,11 @@ import { HeaderPageComponent } from "../../header/header-page/header-page.compon
 import { MainPageComponent } from "../../main-content/main-page/main-page.component";
 import { NavPageComponent } from "../../nav-page/nav-page/nav-page.component";
 import { SidebarComponent } from "../../sidebar-cards/sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderPageComponent, MainPageComponent, NavPageComponent, SidebarComponent],
+  imports: [HeaderPageComponent, MainPageComponent, NavPageComponent, SidebarComponent, RouterOutlet],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
