dfe16449a36e570dda1e4593f0cd22d956aca4b1
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-09-22 18:26:07 -0600

    Fix: Se eliminan lineas inecesarias

--- src/app/components/home-page/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page/home-page.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderPageComponent } from "../../header/header-page/header-page.component";
-import { MainPageComponent } from "../../main-content/main-page/main-page.component";
 import { NavPageComponent } from "../../nav-page/nav-page/nav-page.component";
 import { SidebarComponent } from "../../sidebar-cards/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderPageComponent, MainPageComponent, NavPageComponent, SidebarComponent, RouterOutlet],
+  imports: [HeaderPageComponent, NavPageComponent, SidebarComponent, RouterOutlet],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
